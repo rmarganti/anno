d17c4d4e2c137ce267e0de8c3c6ee64e63706d26
Ryan Marganti <ryan.marganti@gmail.com>
2026-03-29 11:54:29 -0400

    feat: add structured keybinding help content

--- src/keybinds/mod.rs
+++ src/keybinds/mod.rs
@@ -1,7 +1,10 @@
 pub mod handler;
+pub mod help_content;
 pub mod mode;
 
 #[allow(unused_imports)] // TODO: re-exports used by later integration steps
 pub use handler::{Action, KeybindHandler};
+#[allow(unused_imports)]
+pub use help_content::{HelpEntry, HelpSection, help_sections};
 #[allow(unused_imports)]
 pub use mode::Mode;
